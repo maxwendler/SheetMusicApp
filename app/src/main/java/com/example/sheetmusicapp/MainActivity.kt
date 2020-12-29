afe30300685294204e2767e658105f1ba4f91322
maxwendler <max.wendler@mailbox.tu-dresden.de>
2020-12-29 14:38:49 +0100

    Added scaled bar drawable to main activity. Added equalization of drawable path stroke widths after scaling.

--- app/src/main/java/com/example/sheetmusicapp/MainActivity.kt
+++ app/src/main/java/com/example/sheetmusicapp/MainActivity.kt
@@ -1,7 +1,11 @@
 package com.example.sheetmusicapp
 
 import android.os.Bundle
+import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
+import androidx.constraintlayout.widget.ConstraintLayout
+import androidx.core.view.doOnLayout
+import com.devs.vectorchildfinder.VectorChildFinder
 import com.example.sheetmusicapp.scoreModel.Score
 import com.example.sheetmusicapp.scoreModel.TimeSignature
 
@@ -10,6 +14,25 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        // Set callback for resetting stroke widths of bar drawable after scaling.
+        val mainConstraintLayout = findViewById<ConstraintLayout>(R.id.main)
+        mainConstraintLayout.doOnLayout {
+            scaleBarLineStrokeWidth()
+        }
+
         val exampleScore = Score.makeEmpty(bars = 32, timeSignature =  TimeSignature(4, 4))
+    }
+
+    /**
+     * Sets the widths of all strokes of the bar drawable to be equal again, after they changed due to scaling.
+     */
+    fun scaleBarLineStrokeWidth(){
+        val barImageView = findViewById<ImageView>(R.id.barImageView)
+        val barDrawableChildFinder = VectorChildFinder(this, R.drawable.ic_bar, barImageView)
+        val leftVertical = barDrawableChildFinder.findPathByName("leftVertical")
+        val rightVertical = barDrawableChildFinder.findPathByName("rightVertical")
+        val exampleHorizontal = barDrawableChildFinder.findPathByName("exampleHorizontal")
+        leftVertical.strokeWidth = exampleHorizontal.strokeWidth
+        rightVertical.strokeWidth = exampleHorizontal.strokeWidth
     }
 }
