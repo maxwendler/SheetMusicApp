02fc776e494ae730df7cffa41f5b22d92a47a133
maxwendler <max.wendler@mailbox.tu-dresden.de>
2020-12-29 18:20:49 +0100

    Refactoring to enable creation of empty voices.

--- app/src/main/java/com/example/sheetmusicapp/scoreModel/Bar.kt
+++ app/src/main/java/com/example/sheetmusicapp/scoreModel/Bar.kt
@@ -55,7 +55,7 @@ class Bar(var barNr: Int, var timeSignature: TimeSignature, initVoices: Map<Int,
         }
 
         var subgroupPaddingWidthPercent : Double = 0.0
-        var noteWidthPercent : Double
+        val noteWidthPercent : Double
         val numberOfSubgroups = timeSignature.numberOfSubgroups
         val unitsPerSubgroup = timeSignature.subGroupUnits
 
@@ -78,47 +78,85 @@ class Bar(var barNr: Int, var timeSignature: TimeSignature, initVoices: Map<Int,
     }
 
     /**
-     * Creates and appends a [RhythmicInterval] instance which is a rest specified by a [RhythmicLength] to a voice of the [Bar] instance.
-     * Private because it's currently only used for the creation of empty bars by [makeEmpty].
+     * Creates and appends a [RhythmicInterval] instance which is a rest specified by a [RhythmicLength] to a voice of rhythmic intervals.
+     * Private because it's currently only used for the creation of an empty voice by [addEmptyVoice].
      */
-    private fun appendRest(length: RhythmicLength, voice: Int){
-        appendRhythmicInterval(length, mapOf<Int,NoteHeadType>(), voice)
+    private fun appendRest(voiceIntervals: MutableList<RhythmicInterval>, length: RhythmicLength){
+        appendRhythmicInterval(voiceIntervals, length, mapOf<Int,NoteHeadType>())
     }
 
     /**
-     * Creates and appends a [RhythmicInterval] instance, specified by a [RhythmicLength] and some note heads, to a voice of the [Bar] instance.
-     * Private because it's currently only used for the creation of empty bars by [makeEmpty] via [appendRest].
+     * Creates and appends a [RhythmicInterval] instance, specified by a [RhythmicLength] and some note heads, to a voice of rhythmic intervals.
+     * Private because it's only used for the creation of an empty voice by [addEmptyVoice] via [appendRest].
      *
-     * See [RhythmicInterval] for details on the first two parameters.
+     * See [RhythmicInterval] for details on the last two parameters.
      */
-    private fun appendRhythmicInterval(length: RhythmicLength, initNoteHeads: Map<Int, NoteHeadType>, voice: Int){
+    private fun appendRhythmicInterval(voiceIntervals: MutableList<RhythmicInterval>, length: RhythmicLength, initNoteHeads: Map<Int, NoteHeadType>){
 
-        val voiceIntervals : MutableList<RhythmicInterval>? = voices[voice]
-        if (voiceIntervals == null){
-            throw IllegalArgumentException("Voice '$voice' does not exist.")
+        // Get first unit after the currently last interval.
+        var startUnit = 0
+        val lastInterval = voiceIntervals.lastOrNull()
+        if (lastInterval != null){
+            startUnit = lastInterval.startUnit + lastInterval.length.lengthInUnits
         }
-        else {
-            // Get first unit after the currently last interval.
-            var startUnit = 0
-            val lastInterval = voiceIntervals.lastOrNull()
-            if (lastInterval != null){
-                startUnit = lastInterval.startUnit + lastInterval.length.lengthInUnits
-            }
 
-            if (startUnit > timeSignature.units) {
-                throw IllegalArgumentException("The start unit exceeds the length of the bar in units.")
-            }
+        if (startUnit > timeSignature.units) {
+            throw IllegalArgumentException("The start unit exceeds the length of the bar in units.")
+        }
 
-            val endUnit = startUnit + length.lengthInUnits
-            if (endUnit > timeSignature.units){
-                throw IllegalArgumentException("The end unit exceeds the length of the bar in units.")
-            }
+        val endUnit = startUnit + length.lengthInUnits
+        if (endUnit > timeSignature.units){
+            throw IllegalArgumentException("The end unit exceeds the length of the bar in units.")
+        }
 
-            // Create and add interval with width from widthPercentOfRhythmicLength.
-            val interval = RhythmicInterval(length, initNoteHeads, startUnit, widthPercentOfRhythmicLength(length, startUnit))
-            voiceIntervals.add(interval)
+        // Create and add interval with width from widthPercentOfRhythmicLength.
+        val interval = RhythmicInterval(length, initNoteHeads, startUnit, widthPercentOfRhythmicLength(length, startUnit))
+        voiceIntervals.add(interval)
+    }
+
+    /**
+     * Creates a voice with the given "Id", fills it with rests according to the bar's time signature,
+     * and adds it to the voices of the bar instance.
+     * Is private because it's only used when creating an empty bar via [makeEmpty] and when adding a note
+     * in a UI voice mode of a voice that does not exist yet in [addNote].
+     *
+     * @param voice Id of the voice to add.
+     * @throws IllegalArgumentException when the specified voice already exists.
+     */
+    private fun addEmptyVoice(voice : Int){
+
+        if (voices[voice] != null){
+            throw IllegalArgumentException("The given voice does already exist and should not be overwritten.")
+        }
+
+        val newVoiceIntervals = mutableListOf<RhythmicInterval>()
+        var remainingBarUnits = timeSignature.units
+
+        // Add rests the voice until no units remain in the bar.
+        while (remainingBarUnits > 0){
+            for (i in 0..restLengthsForEmptyBars.size){
+                val currentRestLength = restLengthsForEmptyBars[i]
+                if (currentRestLength.lengthInUnits <= remainingBarUnits){
+                    appendRest(newVoiceIntervals, currentRestLength)
+                    remainingBarUnits -= currentRestLength.lengthInUnits
+                    break
+                }
+                if (i == restLengthsForEmptyBars.size){
+                    throw IllegalStateException("No rest length fits the rest of the bar.")
+                }
+            }
+        }
+
+        voices[voice] = newVoiceIntervals
+    }
+
+    // Full implementation in next commit.
+    fun addNote(voice: Int, length: RhythmicLength, type: NoteHeadType, height: Int, intervalIdx: Int){
+        if (voices[voice] == null){
+            addEmptyVoice(voice)
         }
     }
+
 
     companion object {
 
@@ -128,28 +166,10 @@ class Bar(var barNr: Int, var timeSignature: TimeSignature, initVoices: Map<Int,
          * @param barNr The number which the bar would have in a score. Can change later.
          * @param timeSignature The time signature of the bar, which will be filled with rests.
          * @return The "empty" bar instance full of rests.
-         * @throws IllegalStateException When filling the bar went wrong, and no rhythmic length could eventually fill the bar of time signature.
-         * Will happen because of missing rhytmic lengths in restLengthsForEmpty bars or not supported time signatures.
          */
         fun makeEmpty(barNr: Int, timeSignature: TimeSignature): Bar {
-
-            val bar = Bar(barNr, timeSignature, mapOf<Int, MutableList<RhythmicInterval>>(1 to mutableListOf()))
-            var remainingBarUnits = timeSignature.units
-
-            // Add rests the voice until no units remain in the bar.
-            while (remainingBarUnits > 0){
-                for (i in 0..restLengthsForEmptyBars.size){
-                    val currentRestLength = restLengthsForEmptyBars[i]
-                    if (currentRestLength.lengthInUnits <= remainingBarUnits){
-                        bar.appendRest(currentRestLength, 1)
-                        remainingBarUnits -= currentRestLength.lengthInUnits
-                        break
-                    }
-                    if (i == restLengthsForEmptyBars.size){
-                        throw IllegalStateException("No rest length fits the rest of the bar.")
-                    }
-                }
-            }
+            val bar = Bar(barNr, timeSignature, mapOf<Int, MutableList<RhythmicInterval>>())
+            bar.addEmptyVoice(1)
             return bar
         }
     }
